2490c1c265afe548913fcbce4ec72b6767dc80fa
yucissy <cissy_yu@brown.edu>
2016-03-23 16:17:21 -0400

    tried file uplaod

--- templates/upload.js
+++ templates/upload.js
@@ -1,9 +1,36 @@
 $(function() {
+	function loadFile(path, success, error) {
+		var xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState === XMLHttpRequest.DONE) {
+				if (xhr.status === 200) {
+					if (success)
+						success(xhr.responseText);
+				} else {
+					if (error)
+						error(xhr);
+				}
+			}
+		}
+		xhr.open("GET", path, true);
+		xhr.send();
+	}
+
+
+
     $("#upload").click(function() {
-    	var request = new XMLHttpRequest();
-    	request.open('POST', '/examInput', true);
-    	request.setRequestHeader('Content-Type', 'multipart/form-data');
-    	var data = $("#categories").prop('files');
-    	request.send(data);
+    	console.log("path: "+$("#categories").val());
+    	var path = $("#categories").val();
+    	loadFile(path, function(data) {
+    		console.log(data);
+    	}, function(xhr) {
+    		console.error(xhr);
+    	});
+    	// var request = new XMLHttpRequest();
+    	// request.open('POST', '/examInput', true);
+    	// request.setRequestHeader('Content-Type', 'multipart/form-data');
+    	// var data = $("#categories").prop('files');
+    	// console.log(data);
+    	// request.send(data);
 	});
 });
