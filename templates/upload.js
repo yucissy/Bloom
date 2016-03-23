979346ac0b60a89975b88dfd562319e17ce5d0c6
yucissy <cissy_yu@brown.edu>
2016-03-23 16:32:30 -0400

    made csv reader

--- templates/upload.js
+++ templates/upload.js
@@ -1,31 +1,17 @@
 $(function() {
-	function loadFile(path, success, error) {
-		var xhr = new XMLHttpRequest();
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState === XMLHttpRequest.DONE) {
-				if (xhr.status === 200) {
-					if (success)
-						success(xhr.responseText);
-				} else {
-					if (error)
-						error(xhr);
-				}
-			}
-		}
-		xhr.open("GET", path, true);
-		xhr.send();
-	}
+    $("#upload").click(function(e) {
+    	var input = e.target;
+    	var selected = $("#categories").get(0).files[0];
 
+    	var reader = new FileReader();
+    	reader.onload = function() {
 
+    		//this variable holds the csv data!
+    		var dataURL = reader.result;
+    		console.log(dataURL);
+    	};
+    	reader.readAsText(selected);
 
-    $("#upload").click(function() {
-    	console.log("path: "+$("#categories").val());
-    	var path = $("#categories").val();
-    	loadFile(path, function(data) {
-    		console.log(data);
-    	}, function(xhr) {
-    		console.error(xhr);
-    	});
     	// var request = new XMLHttpRequest();
     	// request.open('POST', '/examInput', true);
     	// request.setRequestHeader('Content-Type', 'multipart/form-data');
