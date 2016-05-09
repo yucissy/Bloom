cc383c722d452d644ebb1d9440835ac7c530644f
Katie <katie_han@brown.edu>
2016-05-09 01:16:48 -0400

    removed caching and commented out visualize roster for tab click

--- static/upload.js
+++ static/upload.js
@@ -1,4 +1,3 @@
-var rosterCached = false;
 
 function inputScores(id, studentID) {
 	
@@ -7,109 +6,105 @@ function inputScores(id, studentID) {
 }
 
 function visualizeRoster() {
-    if (!rosterCached) {
-        rosterCached = true;
+	var examTitles = [];
 
-    	var examTitles = [];
+	var user_ID = $("meta[name='user_id']").attr("content");
+	var course_ID = $("meta[name='course_id']").attr("content");
+	var postParameters = {courseID: course_ID, userID: user_ID};
 
-    	var user_ID = $("meta[name='user_id']").attr("content");
-    	var course_ID = $("meta[name='course_id']").attr("content");
-    	var postParameters = {courseID: course_ID, userID: user_ID};
-
-    	var request = new XMLHttpRequest();
-    	request.open('POST', '/getExams', true);
-        request.setRequestHeader('Content-Type', 'application/json');
-        request.send(JSON.stringify(postParameters));
-        request.onreadystatechange = function() {
-            if (request.readyState == 4 && request.status == 200) {
-                var response = JSON.parse(request.responseText);
-                console.log(response);
-                $.each(response.exams, function(i, v) {
-                	examTitles.push({title: v.title, id: v._id});
-                });
-            }
+	var request = new XMLHttpRequest();
+	request.open('POST', '/getExams', true);
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send(JSON.stringify(postParameters));
+    request.onreadystatechange = function() {
+        if (request.readyState == 4 && request.status == 200) {
+            var response = JSON.parse(request.responseText);
+            console.log(response);
+            $.each(response.exams, function(i, v) {
+            	examTitles.push({title: v.title, id: v._id});
+            });
         }
+    }
 
-        var request2 = new XMLHttpRequest();
-    	request2.open('POST', '/getRoster', true);
-        request2.setRequestHeader('Content-Type', 'application/json');
-        request2.send(JSON.stringify(postParameters));
+    var request2 = new XMLHttpRequest();
+	request2.open('POST', '/getRoster', true);
+    request2.setRequestHeader('Content-Type', 'application/json');
+    request2.send(JSON.stringify(postParameters));
 
-        request2.onreadystatechange = function() {
-            if (request2.readyState == 4 && request2.status == 200) {
-                var response = JSON.parse(request2.responseText);
-                console.log(response);
-                $('#part2').empty();
-                var table = d3.select("#part2")
-                	.append("div")
-                	.attr('class', 'table-responsive')
-                	.append("table")
-                	.attr('class', 'table-hover');
+    request2.onreadystatechange = function() {
+        if (request2.readyState == 4 && request2.status == 200) {
+            var response = JSON.parse(request2.responseText);
+            console.log(response);
+            $('#part2').empty();
+            var table = d3.select("#part2")
+            	.append("div")
+            	.attr('class', 'table-responsive')
+            	.append("table")
+            	.attr('class', 'table-hover');
 
-                var thead = table.append('thead')
-                	.append('tr');
+            var thead = table.append('thead')
+            	.append('tr');
 
-                thead.append('th').text('Name');
-                $.each(examTitles, function(i,v) {
-                	thead.append('th').text(v.title)
-                		.attr('id', v.id);
-                });
+            thead.append('th').text('Name');
+            $.each(examTitles, function(i,v) {
+            	thead.append('th').text(v.title)
+            		.attr('id', v.id);
+            });
 
-                var tbody = table.append('tbody');
-                $.each(response.roster, function(i,v) {
-                	var trow = tbody.append('tr').attr('id', v._id);
-                	trow.append('td').text(v.name);
-                	var studentID = v._id;
-                	var studentName = v.name;
-                	$.each(v.exams, function(i,v) {
-                		if (v[examTitles[i].title] == true)
-                			trow.append('td').attr('class','y');
-                		else
-                			trow.append('td').append('button')
-                				.attr('type', 'button')
-                				.attr('class', 'btn btn-secondary-outline')
-                				.on('click', function() {
-                					
-                					var index = $(this).closest('td').index();
-                					var examID = $('th').eq(index).attr('id');
-                					$('#submitLine').empty();
-                					$('#submitLine').html('Submit '+studentName+"'s scores:");
-                					inputScores(examID, studentID);
-                				})
-                				.append('span')
-                				.attr('class', 'glyphicon glyphicon-pencil')
-                				.attr('aria-hidden', 'true');
-                	});
+            var tbody = table.append('tbody');
+            $.each(response.roster, function(i,v) {
+            	var trow = tbody.append('tr').attr('id', v._id);
+            	trow.append('td').text(v.name);
+            	var studentID = v._id;
+            	var studentName = v.name;
+            	$.each(v.exams, function(i,v) {
+            		if (v[examTitles[i].title] == true)
+            			trow.append('td').attr('class','y');
+            		else
+            			trow.append('td').append('button')
+            				.attr('type', 'button')
+            				.attr('class', 'btn btn-secondary-outline')
+            				.on('click', function() {
+            					
+            					var index = $(this).closest('td').index();
+            					var examID = $('th').eq(index).attr('id');
+            					$('#submitLine').empty();
+            					$('#submitLine').html('Submit '+studentName+"'s scores:");
+            					inputScores(examID, studentID);
+            				})
+            				.append('span')
+            				.attr('class', 'glyphicon glyphicon-pencil')
+            				.attr('aria-hidden', 'true');
+            	});
 
-                });
+            });
 
-                var button = d3.select("#part2")
-                	.append("button")
-                	.attr('type', 'button')
-                	.attr('class', 'btn btn-default')
-                	.attr('aria-label', 'Left Align')
-                	.attr('id', 'course_report');
+            var button = d3.select("#part2")
+            	.append("button")
+            	.attr('type', 'button')
+            	.attr('class', 'btn btn-default')
+            	.attr('aria-label', 'Left Align')
+            	.attr('id', 'course_report');
 
-                button.append('span')
-                	.attr('class', 'glyphicon glyphicon-download')
-                	.attr('aria-hidden', 'true');
+            button.append('span')
+            	.attr('class', 'glyphicon glyphicon-download')
+            	.attr('aria-hidden', 'true');
 
-                button.append('span')
-                	.attr('class', 'btn-text')
-                	.text('Course Report');
+            button.append('span')
+            	.attr('class', 'btn-text')
+            	.text('Course Report');
 
 
-                $('#course_report').on('click', function() {
-                    var user_ID = $("meta[name='user_id']").attr("content");
-                    var course_ID = $("meta[name='course_id']").attr("content");
-                    console.log('here');
-                    $form = $('<form action="/downloadAggregate" method="POST"></form>');
-                    $form.append("<input type='hidden' name='courseID' value='"+course_ID+"'/>");
-                    $form.append("<input type='hidden' name='userID' value='"+user_ID+"'/>");
-                    $form.submit();
+            $('#course_report').on('click', function() {
+                var user_ID = $("meta[name='user_id']").attr("content");
+                var course_ID = $("meta[name='course_id']").attr("content");
+                console.log('here');
+                $form = $('<form action="/downloadAggregate" method="POST"></form>');
+                $form.append("<input type='hidden' name='courseID' value='"+course_ID+"'/>");
+                $form.append("<input type='hidden' name='userID' value='"+user_ID+"'/>");
+                $form.submit();
 
-                });
-            }
+            });
         }
     }
 }
@@ -139,7 +134,6 @@ $(function() {
                     var response = JSON.parse(request.responseText);
                     if (response.status == 'success') {
                         $('#myModal').modal('hide');
-                        rosterCached = false;
                         visualizeRoster();
                     } else {
                         $('#modal-alert').text('Something went wrong :(');
